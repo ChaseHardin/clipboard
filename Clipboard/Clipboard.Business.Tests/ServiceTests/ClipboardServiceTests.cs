421bf3143fea34cdae6467a05f1ab44deabf2da9
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2016-12-20 20:19:59 -0600

    Setup Business layer with tests.

---
+++ Clipboard/Clipboard.Business.Tests/ServiceTests/ClipboardServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Clipboard.Business.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Clipboard.Business.Tests.ServiceTests
+{
+    [TestClass]
+    public class ClipboardServiceTests
+    {
+        [TestMethod]
+        public void GetDataFromClipboard_ClipboardHasData_ShouldReturnDataOnClipboard()
+        {
+            System.Windows.Clipboard.SetText("Test data getting set.");
+            var clipboardService = new ClipboardService();
+
+            Assert.AreEqual("Test data getting set.", clipboardService.GetTextFromClipboard());
+        }
+
+        [TestMethod]
+        public void AddTextToDictionary_HaveTextFromClipboard_ShouldHaveNewItemInDictionary()
+        {
+            var clipboardService = new ClipboardService();
+            var dictionary = clipboardService.ClipboardDirectory;
+            
+            System.Windows.Clipboard.SetText("Adding item to dictionary.");
+            var firstCopy = clipboardService.AddTextToDictionary(clipboardService.GetTextFromClipboard(), dictionary);
+            
+            Assert.AreEqual(1, firstCopy.Count);
+            Assert.AreEqual("Adding item to dictionary.", firstCopy.First().Value);
+           
+            System.Windows.Clipboard.SetText("Second item to dictionary.");
+            var secondCopy = clipboardService.AddTextToDictionary(clipboardService.GetTextFromClipboard(), dictionary);
+
+            Assert.AreEqual(2, secondCopy.Count);
+            Assert.AreEqual("Second item to dictionary.", secondCopy.First(x => x.Key == 2).Value);
+        }
+    }
+} 
---
+++ Clipboard/Clipboard.Business/Services/ClipboardService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Clipboard.Business.Services
+{
+    public class ClipboardService
+    {
+        public Dictionary<int, string> ClipboardDirectory = new Dictionary<int, string>();
+
+        public string GetTextFromClipboard()
+        {
+            return System.Windows.Clipboard.GetText();
+        }
+
+        public Dictionary<int, string> AddTextToDictionary(string getTextFromClipboard, Dictionary<int, string> dictionary)
+        {
+            dictionary.Add(dictionary.Count + 1, getTextFromClipboard);
+            return dictionary;
+        }
+    }
+}
